763aa880df2b9005813c732f797a4ac59e27ed68
wjzw136 <80760465@qq.com>
2019-05-07 15:23:16 +0800

    22
    222

---
+++ script/public.js
@@ -0,0 +1,36 @@
+var Group = {
+    'SY_BOX': { 'header': 'SY_header', 'Group': 'SY_Group' },
+    'FL_BOX': { 'header': 'FL_header', 'Group': 'FL_Group' },
+    'ME_BOX': { 'header': 'ME_header', 'Group': 'ME_Group' }
+}
+var FL_GroupHtml = ['FL_zonghe', 'FL_mhxy','FL_dnf'];
+
+function hiddenframegroup(frame, framegroup) {
+    for (var i = 0; i < frame.length; i++) {
+        api.setFrameAttr({
+            name: frame[i],
+            hidden: true
+        });
+    }
+    for (var i = 0; i < framegroup.length; i++) {
+        api.setFrameGroupAttr({
+            name: framegroup[i],
+            hidden: true
+        });
+    }
+}
+
+function showframegroup(frame, framegroup) {
+    for (var i = 0; i < frame.length; i++) {
+        api.setFrameAttr({
+            name: frame[i],
+            hidden: false
+        });
+    }
+    for (var i = 0; i < framegroup.length; i++) {
+        api.setFrameGroupAttr({
+            name: framegroup[i],
+            hidden: false
+        });
+    }
+}
